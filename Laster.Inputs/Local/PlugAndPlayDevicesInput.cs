7605857f31491c071fa9b761c677eabb9889ea73
@ShargonXL <shargon@gmail.com>
2016-09-29 14:20:30 +0200

    Pnp

--- Laster.Inputs/Local/PlugAndPlayDevicesInput.cs
+++ Laster.Inputs/Local/PlugAndPlayDevicesInput.cs
@@ -15,64 +15,104 @@ namespace Laster.Inputs.Local
     public class PlugAndPlayDevicesInput : IDataInput
     {
         // http://stackoverflow.com/questions/3331043/get-list-of-connected-usb-devices
-        public class PnpDevice
+
+        public class LogicalDevice
         {
             #region Propiedades
-            public int Availability { get; set; }
+
+            // https://msdn.microsoft.com/en-us/library/aa387884(v=vs.85).aspx
+
+            //string Caption;
+            //string Description;
+            //datetime InstallDate;
+            //string Name;
+            //string Status;
+            //uint16 Availability;
+            //uint32 ConfigManagerErrorCode;
+            //boolean ConfigManagerUserConfig;
+            //string CreationClassName;
+            //string DeviceID;
+            //uint16 PowerManagementCapabilities[];
+            //boolean ErrorCleared;
+            //string ErrorDescription;
+            //uint32 LastErrorCode;
+            //string PNPDeviceID;
+            //boolean PowerManagementSupported;
+            //uint16 StatusInfo;
+            //string SystemCreationClassName;
+            //string SystemName;
+
             public string Caption { get; set; }
-            public string ClassGuid { get; set; }
-            public string[] CompatibleID { get; set; }
-            public long ConfigManagerErrorCode { get; set; }
-            public bool ConfigManagerUserConfig { get; set; }
-            public string CreationClassName { get; set; }
             public string Description { get; set; }
-            public string DeviceID { get; set; }
-            public bool ErrorCleared { get; set; }
-            public string ErrorDescription { get; set; }
-            public string[] HardwareID { get; set; }
             public DateTime InstallDate { get; set; }
-            public long LastErrorCode { get; set; }
-            public string Manufacturer { get; set; }
             public string Name { get; set; }
-            public string PNPClass { get; set; }
-            public string PNPDeviceID { get; set; }
-            public int[] PowerManagementCapabilities { get; set; }
-            public bool PowerManagementSupported { get; set; }
-            public bool Present { get; set; }
-            public string Service { get; set; }
             public string Status { get; set; }
-            public int StatusInfo { get; set; }
+            //public int Availability { get; set; }
+            //public ulong ConfigManagerErrorCode { get; set; }
+            //public bool ConfigManagerUserConfig { get; set; }
+            public string CreationClassName { get; set; }
+            public string DeviceID { get; set; }
+            //public uint[] PowerManagementCapabilities { get; set; }
+            //public bool ErrorCleared { get; set; }
+            public string ErrorDescription { get; set; }
+            public ulong LastErrorCode { get; set; }
+            public string PNPDeviceID { get; set; }
+            public bool PowerManagementSupported { get; set; }
+            //public int StatusInfo { get; set; }
             public string SystemCreationClassName { get; set; }
             public string SystemName { get; set; }
             #endregion
 
-            public PnpDevice(ManagementBaseObject queryObj)
+            public LogicalDevice(ManagementBaseObject queryObj)
             {
-                Availability = Convert.ToInt32(CheckNull(queryObj["Availability"], -1));
+                //Availability = Convert.ToInt32(CheckNull(queryObj["Availability"], (int)0));
                 Caption = CheckNull(queryObj["Caption"], "").ToString();
-                ClassGuid = CheckNull(queryObj["ClassGuid"], "").ToString();
-                CompatibleID = (string[])CheckNull(queryObj["CompatibleId"], null);
-                ConfigManagerErrorCode = Convert.ToInt64(CheckNull(queryObj["ConfigManagerErrorCode"], -1));
-                ConfigManagerUserConfig = Convert.ToBoolean(CheckNull(queryObj["ConfigManagerUserConfig"], false));
+                //ConfigManagerUserConfig = Convert.ToBoolean(CheckNull(queryObj["ConfigManagerUserConfig"], false));
                 CreationClassName = CheckNull(queryObj["CreationClassName"], "").ToString();
                 Description = CheckNull(queryObj["Description"], "").ToString();
-                DeviceID = CheckNull(queryObj["DeviceID"], "").ToString();
-                ErrorCleared = Convert.ToBoolean(CheckNull(queryObj["ErrorCleared"], false));
-                HardwareID = (string[])CheckNull(queryObj["HardwareID"], null);
+                //ErrorCleared = Convert.ToBoolean(CheckNull(queryObj["ErrorCleared"], false));
+                ErrorDescription = CheckNull(queryObj["ErrorDescription"], "").ToString();
                 InstallDate = Convert.ToDateTime(CheckNull(queryObj["InstallDate"], DateTime.MinValue));
-                LastErrorCode = Convert.ToInt64(CheckNull(queryObj["LastErrorCode"], -1));
-                Manufacturer = CheckNull(queryObj["Manufacturer"], "").ToString();
-                Name = CheckNull(queryObj["Name"], "").ToString();
-                PNPClass = CheckNull(queryObj["PNPClass"], "").ToString();
+                LastErrorCode = Convert.ToUInt64(CheckNull(queryObj["LastErrorCode"], (ulong)0));
                 PNPDeviceID = CheckNull(queryObj["PNPDeviceID"], "").ToString();
-                PowerManagementCapabilities = (int[])CheckNull(queryObj["PowerManagementCapabilities"], null);
+                //PowerManagementCapabilities = (uint[])CheckNull(queryObj["PowerManagementCapabilities"], null);
                 PowerManagementSupported = Convert.ToBoolean(CheckNull(queryObj["PowerManagementSupported"], false));
-                Present = Convert.ToBoolean(CheckNull(queryObj["Present"], false));
-                Service = CheckNull(queryObj["Service"], "").ToString();
                 Status = CheckNull(queryObj["Status"], "").ToString();
-                StatusInfo = Convert.ToInt32(CheckNull(queryObj["StatusInfo"], -1));
+                //StatusInfo = Convert.ToInt32(CheckNull(queryObj["StatusInfo"], (int)0));
                 SystemCreationClassName = CheckNull(queryObj["SystemCreationClassName"], "").ToString();
                 SystemName = CheckNull(queryObj["SystemName"], "").ToString();
+                //ConfigManagerErrorCode = Convert.ToUInt64(CheckNull(queryObj["ConfigManagerErrorCode"], (ulong)0));
+                DeviceID = CheckNull(queryObj["DeviceID"], "").ToString();
+                Name = CheckNull(queryObj["Name"], "").ToString();
+            }
+            public T CheckNull<T>(object v1, T v2)
+            {
+                if (v1 == null) return v2;
+                return (T)v1;
+            }
+        }
+
+        public class PnpDevice : LogicalDevice
+        {
+            #region Propiedades
+            public string ClassGuid { get; set; }
+            public string[] CompatibleID { get; set; }
+            public string[] HardwareID { get; set; }
+            public string Manufacturer { get; set; }
+            public string PNPClass { get; set; }
+            public bool Present { get; set; }
+            public string Service { get; set; }
+            #endregion
+
+            public PnpDevice(ManagementBaseObject queryObj) : base(queryObj)
+            {
+                ClassGuid = CheckNull(queryObj["ClassGuid"], "").ToString();
+                CompatibleID = CheckNull<string[]>(queryObj["CompatibleId"], null);
+                HardwareID = CheckNull<string[]>(queryObj["HardwareID"], null);
+                Manufacturer = CheckNull(queryObj["Manufacturer"], "").ToString();
+                PNPClass = CheckNull(queryObj["PNPClass"], "").ToString();
+                Present = Convert.ToBoolean(CheckNull(queryObj["Present"], false));
+                Service = CheckNull(queryObj["Service"], "").ToString();
             }
 
             // Disable (Requeire x86/x64
@@ -115,26 +155,55 @@ namespace Laster.Inputs.Local
                 }
             }
 
-            object CheckNull(object v1, object v2)
-            {
-                if (v1 == null) return v2;
-                return v1;
-            }
-
             public override string ToString() { return DeviceID; }
         }
 
-        ManagementEventWatcher removeWatcher, insertWatcher;
-        List<PnpDevice> items = new List<PnpDevice>();
+        public class UsbDevice : LogicalDevice
+        {
+            #region Propiedades
+            //public ushort ClassCode { get; set; }
+            //public ushort[] CurrentAlternateSettings { get; set; }
+            //public ushort CurrentConfigValue { get; set; }
+            //public bool GangSwitched { get; set; }
+            //public ushort NumberOfConfigs { get; set; }
+            //public ushort NumberOfPorts { get; set; }
+            //public ushort ProtocolCode { get; set; }
+            //public ushort SubclassCode { get; set; }
+            //public uint USBVersion { get; set; }
+            #endregion
 
-        [DefaultValue(EReturn.DeviceID)]
-        public EReturn Return { get; set; }
+            public UsbDevice(ManagementBaseObject queryObj) : base(queryObj)
+            {
+                //ClassCode = Convert.ToUInt16(CheckNull(queryObj["ClassCode"], (ushort)0));
+                //CurrentAlternateSettings = (ushort[])CheckNull(queryObj["CurrentAlternateSettings"], null);
+                //CurrentConfigValue = Convert.ToUInt16(CheckNull(queryObj["CurrentConfigValue"], (ushort)0));
+                //ConfigManagerUserConfig = Convert.ToBoolean(CheckNull(queryObj["ConfigManagerUserConfig"], false));
+                //GangSwitched = Convert.ToBoolean(CheckNull(queryObj["GangSwitched"], false));
+                //NumberOfConfigs = Convert.ToUInt16(CheckNull(queryObj["NumberOfConfigs"], (ushort)0));
+                //NumberOfPorts = Convert.ToUInt16(CheckNull(queryObj["NumberOfPorts"], (ushort)0));
+                //ProtocolCode = Convert.ToUInt16(CheckNull(queryObj["ProtocolCode"], (ushort)0));
+                //SubclassCode = Convert.ToUInt16(CheckNull(queryObj["SubclassCode"], (ushort)0));
+                //USBVersion = Convert.ToUInt32(CheckNull(queryObj["USBVersion"], (uint)0));
+            }
+            public override string ToString() { return DeviceID; }
+        }
 
         public enum EReturn
         {
-            DeviceID,
-            Object,
+            // Plug&Play
+            PnpDeviceID,
+            PnpObject,
+
+            // Usb
+            UsbDeviceID,
+            UsbObject,
         }
+
+        ManagementEventWatcher removeWatcher, insertWatcher;
+        List<LogicalDevice> items = new List<LogicalDevice>();
+
+        [DefaultValue(EReturn.PnpDeviceID)]
+        public EReturn Return { get; set; }
 
         public override string Title { get { return "Local - Plug&Play devices"; } }
 
@@ -150,29 +219,47 @@ namespace Laster.Inputs.Local
         {
             switch (Return)
             {
-                case EReturn.DeviceID:
+                case EReturn.PnpDeviceID:
+                case EReturn.UsbDeviceID:
                     {
                         List<string> ls = new List<string>();
-                        foreach (PnpDevice p in items.ToArray()) ls.Add(p.DeviceID);
+                        foreach (LogicalDevice p in items.ToArray()) ls.Add(p.DeviceID);
                         return Reduce(EZeroEntries.Empty, ls.ToArray());
                     }
                 default:
-                case EReturn.Object: return Reduce(EZeroEntries.Empty, items.ToArray());
+                case EReturn.PnpObject:
+                case EReturn.UsbObject: return Reduce(EZeroEntries.Empty, items.ToArray());
             }
         }
         protected override void OnStart()
         {
             // Fill PnpDevices
             items.Clear();
+
+            string obj = "Win32_PnPEntity";
+            switch (Return)
+            {
+                case EReturn.PnpDeviceID:
+                case EReturn.PnpObject: { obj = "Win32_PnPEntity"; break; }
+                case EReturn.UsbDeviceID:
+                case EReturn.UsbObject: { obj = "Win32_USBHub"; break; }
+            }
+
             ManagementObjectSearcher searcher = null;
             try
             {
-                searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity");
+                searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM " + obj);
 
                 foreach (ManagementObject queryObj in searcher.Get())
                     try
                     {
-                        items.Add(new PnpDevice(queryObj));
+                        switch (Return)
+                        {
+                            case EReturn.PnpDeviceID:
+                            case EReturn.PnpObject: { items.Add(new PnpDevice(queryObj)); break; }
+                            case EReturn.UsbDeviceID:
+                            case EReturn.UsbObject: { items.Add(new UsbDevice(queryObj)); break; }
+                        }
                     }
                     catch { }
                     finally { queryObj.Dispose(); }
@@ -188,53 +275,86 @@ namespace Laster.Inputs.Local
             }
 
             // Start waiting changes
-            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_PnPEntity'");
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA '" + obj + "'");
 
             insertWatcher = new ManagementEventWatcher(insertQuery);
             insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
             insertWatcher.Start();
 
             // TargetInstance ISA 'Win32_USBHub' OR 
-            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_PnPEntity'");
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA '" + obj + "'");
             removeWatcher = new ManagementEventWatcher(removeQuery);
             removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
             removeWatcher.Start();
         }
-
         void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
         {
-            PnpDevice p = new PnpDevice((ManagementBaseObject)e.NewEvent["TargetInstance"]);
-            PnpDevice d = Find(p);
-            if (d == null)
+            bool raise = false;
+            switch (Return)
             {
-                items.Add(p);
+                case EReturn.PnpDeviceID:
+                case EReturn.PnpObject:
+                    {
 
-                p.Disable();
-
-                if (RaiseMode != null && RaiseMode is ITriggerRaiseMode)
-                    ((ITriggerRaiseMode)RaiseMode).RaiseTrigger(EventArgs.Empty);
+                        PnpDevice p = new PnpDevice((ManagementBaseObject)e.NewEvent["TargetInstance"]);
+                        PnpDevice d = Find(p);
+                        if (d == null)
+                        {
+                            items.Add(p);
+                            raise = true;
+                        }
+                        break;
+                    }
+                case EReturn.UsbDeviceID:
+                case EReturn.UsbObject:
+                    {
+                        UsbDevice p = new UsbDevice((ManagementBaseObject)e.NewEvent["TargetInstance"]);
+                        UsbDevice d = Find(p);
+                        if (d == null)
+                        {
+                            items.Add(p);
+                            raise = true;
+                        }
+                        break;
+                    }
             }
+
+            if (raise && RaiseMode != null && RaiseMode is ITriggerRaiseMode)
+                ((ITriggerRaiseMode)RaiseMode).RaiseTrigger(EventArgs.Empty);
         }
         void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
         {
-            PnpDevice p = new PnpDevice((ManagementBaseObject)e.NewEvent["TargetInstance"]);
-            PnpDevice d = Find(p);
-            if (d != null)
+            bool raise = false;
+            switch (Return)
             {
-                items.Remove(d);
-
-                if (RaiseMode != null && RaiseMode is ITriggerRaiseMode)
-                    ((ITriggerRaiseMode)RaiseMode).RaiseTrigger(EventArgs.Empty);
+                case EReturn.PnpDeviceID:
+                case EReturn.PnpObject:
+                    {
+                        PnpDevice p = new PnpDevice((ManagementBaseObject)e.NewEvent["TargetInstance"]);
+                        PnpDevice d = Find(p);
+                        if (d != null)
+                        {
+                            items.Remove(d);
+                            raise = true;
+                        }
+                        break;
+                    }
+                case EReturn.UsbDeviceID:
+                case EReturn.UsbObject:
+                    {
+                        UsbDevice p = new UsbDevice((ManagementBaseObject)e.NewEvent["TargetInstance"]);
+                        UsbDevice d = Find(p);
+                        if (d != null)
+                        {
+                            items.Remove(d);
+                            raise = true;
+                        }
+                        break;
+                    }
             }
-        }
-        PnpDevice Find(PnpDevice p)
-        {
-            lock (items)
-                foreach (PnpDevice cur in items)
-                {
-                    if (cur.DeviceID == p.DeviceID) return cur;
-                }
-            return null;
+
+            if (raise && RaiseMode != null && RaiseMode is ITriggerRaiseMode)
+                ((ITriggerRaiseMode)RaiseMode).RaiseTrigger(EventArgs.Empty);
         }
         protected override void OnStop()
         {
@@ -256,6 +376,15 @@ namespace Laster.Inputs.Local
             }
 
             base.OnStop();
+        }
+        T Find<T>(T p) where T : LogicalDevice
+        {
+            lock (items)
+                foreach (LogicalDevice cur in items)
+                {
+                    if (cur.DeviceID == p.DeviceID) return (T)cur;
+                }
+            return null;
         }
     }
 }
